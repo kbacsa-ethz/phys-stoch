b0fde4b5496851858b071d4d5b68284ec28de6be
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-04-26 16:32:41 +0800

    added RNN utils

---
+++ utils.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+# this function takes a torch mini-batch and reverses each sequence
+# (w.r.t. the temporal axis, i.e. axis=1).
+def reverse_sequences(mini_batch, seq_lengths):
+    reversed_mini_batch = torch.zeros_like(mini_batch)
+    for b in range(mini_batch.size(0)):
+        T = seq_lengths[b]
+        time_slice = torch.arange(T - 1, -1, -1, device=mini_batch.device)
+        reversed_sequence = torch.index_select(mini_batch[b, :, :], 0, time_slice)
+        reversed_mini_batch[b, 0:T, :] = reversed_sequence
+
+    return reversed_mini_batch
+
+
+# this function takes the hidden state as output by the PyTorch rnn and
+# unpacks it it; it also reverses each sequence temporally
+def pad_and_reverse(rnn_output, seq_lengths):
+    rnn_output, _ = nn.utils.rnn.pad_packed_sequence(rnn_output, batch_first=True)
+    reversed_output = reverse_sequences(rnn_output, seq_lengths)
+    return reversed_output
