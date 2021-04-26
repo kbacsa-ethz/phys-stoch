c6acd79363e6ff1c7ef75dca026d706df2e9bf55
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-04-26 16:32:24 +0800

    convert to tensor

--- phys_data.py
+++ phys_data.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
 class TrajectoryDataset(Dataset):
     def __init__(self, root_dir, exp_dir, input_dofs, sequence_length):
-
         self.states = np.load(os.path.join(root_dir, exp_dir, 'state.npy'))
         self.observations = np.load(os.path.join(root_dir, exp_dir, 'obs.npy'))
         self.forces = np.load(os.path.join(root_dir, exp_dir, 'force.npy'))
@@ -15,9 +15,12 @@ class TrajectoryDataset(Dataset):
         self.n_dof = self.n_states // 3
         self.input_dofs = input_dofs
         self.n_obs = self.observations.shape[-1]
-        self.states = np.reshape(self.states, [self.n_exp * self.n_time//sequence_length, sequence_length, self.n_states])
-        self.observations = np.reshape(self.observations, [self.n_exp * self.n_time//sequence_length, sequence_length, self.n_obs])
-        self.forces = np.reshape(self.forces, [self.n_exp * self.n_time//sequence_length, sequence_length, self.n_dof])
+        self.states = torch.from_numpy(
+            np.reshape(self.states, [self.n_exp * self.n_time // sequence_length, sequence_length, self.n_states]))
+        self.observations = torch.from_numpy(
+            np.reshape(self.observations, [self.n_exp * self.n_time // sequence_length, sequence_length, self.n_obs]))
+        self.forces = torch.from_numpy(
+            np.reshape(self.forces, [self.n_exp * self.n_time // sequence_length, sequence_length, self.n_dof]))
 
     def __len__(self):
         return self.n_exp * self.n_time // self.seq_len
