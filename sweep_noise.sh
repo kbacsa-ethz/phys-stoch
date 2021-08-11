5b69923de14a7715af36ae15385524068baf5071
Kiran Bacsa <kbacsa-ethz>
2021-08-11 15:18:55 +0800

    correct seq arguments

--- sweep_noise.sh
+++ sweep_noise.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for i in $(seq 0.01 0.03 0.06 0.1 0.13 0.16 0.2)
+for i in $(seq 0.0 0.05 0.3)
 do
-  python gen_config.py -rp "$PWD" -type "free" -ndof 2 -noise "$i"
-  python simulation.py --root-path "$PWD"
-  python train.py --root-path="$PWD" -in 4 -z 4 -enc 4 -sq 50 -n 100 -wd 0.01 --headless --comet
+  #python gen_config.py -rp "$PWD" -type "free" -ndof 2 -noise "$i"
+  #python simulation.py --root-path "$PWD"
+  #python train.py --root-path="$PWD" -in 4 -z 4 -enc 4 -sq 50 -n 100 -wd 0.01 --headless --comet
+  echo "$i"
 done
