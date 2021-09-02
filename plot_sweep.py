7d87109eedb17fe094fb197e4e9ffca2179a9dc8
Kiran Bacsa <kbacsa-ethz>
2021-09-02 10:51:27 +0800

    changed plot to scatter for sweep

--- plot_sweep.py
+++ plot_sweep.py
@@ -2,6 +2,7 @@ from comet_ml import Experiment
 
 import argparse
 import os
+import json
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -14,43 +15,34 @@ def plot_sweep(cfg):
     else:
         mpl.use('TkAgg')
 
-    experiment = Experiment(project_name="phys-sweep", api_key="Bm8mJ7xbMDa77te70th8PNcT8")
+    experiment = Experiment(project_name="2dof", api_key="Bm8mJ7xbMDa77te70th8PNcT8")
 
-    data = pd.read_csv(os.path.join(cfg.root_path, "sweeps", cfg.parameter + '.txt'), sep=',', header=None)
-    data = pd.DataFrame(data)
+    param_sweep = ["emission_dim", "emission_layers", "transmission_dim", "potential_hidden", "potential_layers", "encoder_layers"]
 
-    x = data[0]
-    y = data[1]
-    z = data[2]
+    exp = []
+    file_path = os.path.join(cfg.root_path, "sweeps", "2dof.txt")
+    with open(file_path) as file:
+        while line := file.readline().rstrip():
+            out = json.loads(line)
+            df = pd.DataFrame.from_dict(out.items())
+            df.set_index(0, inplace=True)
+            exp.append(df)
 
-    fig = plt.figure(figsize=(16, 7))
-    plt.title(cfg.parameter)
-    plt.plot(x, y, 'r')
-    plt.xlabel("parameter value")
-    plt.ylabel("AIC")
-    experiment.log_figure(figure=fig, figure_name=cfg.parameter)
+    df_final = pd.concat(exp, axis=1)
+    df_final = df_final.transpose()
 
-    fig = plt.figure(figsize=(16, 7))
-    plt.title(cfg.parameter)
-    plt.plot(x, z, 'r')
-    plt.xlabel("parameter value")
-    plt.ylabel("MSE")
-    experiment.log_figure(figure=fig, figure_name=cfg.parameter)
-    return 0
+    for param in param_sweep:
+        df_final.plot.scatter(param, "mse")
+        plt.title("Sweep of {}".format(param))
+        experiment.log_figure(figure_name=param)
 
 
 if __name__ == '__main__':
     # parse config
     parser = argparse.ArgumentParser(description="parse args")
-
-    # I/O parameters
     parser.add_argument('--root-path', type=str, default='.')
-
-    # Sweep parameters
-    parser.add_argument('--parameter', type=str, default='emission_dim')
 
     # Machine parameters
     parser.add_argument('--headless', action='store_true')
-
     args = parser.parse_args()
     plot_sweep(args)
