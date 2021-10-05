0715f8fd9ee4652d645ee251f58dfc24a362b857
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-05 08:33:40 +0200

    added script for duffing oscillator on euler

---
+++ cluster_scripts/duffing.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2-noise 0.05
+python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini"
+python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini" \
+ -sq 50 -n 30 -wd 0.01 \
+ --headless --comet
