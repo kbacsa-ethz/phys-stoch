aa4b03f07318a87c599e2bd0921f38dca4700ac3
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-11 14:33:11 +0200

    add best architecture for duffing

--- cluster_scripts/duffing.sh
+++ cluster_scripts/duffing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.05
+python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.20 -n_iter 2000
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini" \
+ -e 14 -ne 3 -tr 26 -ph 23 -pl 4 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
  --headless --comet
