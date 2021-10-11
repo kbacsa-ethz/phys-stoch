25eb422b9a749aa9dffafb9fa03c779561b94c1f
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-11 14:36:28 +0200

    optimal parameters

--- cluster_scripts/pendulum.sh
+++ cluster_scripts/pendulum.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics pendulum -ndof 2 -noise 0.05
+python gen_config.py -rp "$PWD" -type free -dynamics pendulum -ndof 2 -noise 0.20
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_pendulum_free.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_pendulum_free.ini" \
  -sq 50 -n 30 -wd 0.01 \
+ -e 15 -ne 1 -tr 24 -ph 65 -pl 1 -nenc 1 \
  --headless --comet
