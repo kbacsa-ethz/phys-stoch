307592800d9b7489824265c85d0c849e29c6040a
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-12 15:47:49 +0200

    adapt bounds for pendulum phase-space

--- cluster_scripts/sweep_pendulum.sh
+++ cluster_scripts/sweep_pendulum.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics pendulum -ndof 2 -noise 0.05 -n_iter 500
+python gen_config.py -rp "$PWD" -type free -dynamics pendulum -ndof 2 -noise 0.20 -n_iter 500  \
+-l_x -3.141592653589793 -u_x 3.141592653589793
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_pendulum_free.ini"
 python comet_sweep.py --root-path "$PWD" --config-path "config/2springmass_pendulum_free.ini" \
  -sq 50 -n 30 -wd 0.01 \
