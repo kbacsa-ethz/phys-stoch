cbcb64f07bc8e05c538effc3c0094b7137db4172
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-26 10:41:42 +0200

    add script for linear dynamics

---
+++ cluster_scripts/linear.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python gen_config.py -rp "$PWD" -type free -dynamics linear -ndof 2 -noise 0.20 -n_iter 500
+python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini"
+python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini" \
+ -e 16 -ne 0 -tr 32 -ph 60 -pl 2 -nenc 2 \
+ -sq 50 -n 30 -wd 0.01 \
+ --headless --comet
