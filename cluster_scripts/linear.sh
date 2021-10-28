c87cddf90b279d535bb854304e1c61bcf38163bf
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-28 09:59:49 +0200

    change linear to optimal parameters

--- cluster_scripts/linear.sh
+++ cluster_scripts/linear.sh
@@ -2,6 +2,6 @@
 python gen_config.py -rp "$PWD" -type free -dynamics linear -ndof 2 -noise 0.20 -n_iter 500
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini" \
- -e 16 -ne 0 -tr 32 -ph 60 -pl 2 -nenc 2 \
+ -e 18 -ne 2 -tr 15 -ph 85 -pl 2 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
  --headless --comet
--- cluster_scripts/linear_dissipative.sh
+++ cluster_scripts/linear_dissipative.sh
@@ -3,6 +3,6 @@ python gen_config.py -rp "$PWD" -type free -dynamics linear -type dissipative -n
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_dissipative.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_linear_dissipative.ini" \
  --dissipative \
- -e 16 -ne 0 -tr 32 -ph 60 -pl 2 -nenc 2 \
+ -e 18 -ne 2 -tr 15 -ph 85 -pl 2 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
  --headless --comet
