5db24062ffdd8bf3537e43129e269dd04dad346a
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-11-23 09:33:04 +0100

    fix interpreter

--- cluster_scripts/duffing_dissipative.sh
+++ cluster_scripts/duffing_dissipative.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 python gen_config.py -rp "$PWD" -type dissipative -dynamics -ext free duffing -ndof 2 -noise 0.20 -n_iter 500
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_dissipative.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_dissipative.ini" \
