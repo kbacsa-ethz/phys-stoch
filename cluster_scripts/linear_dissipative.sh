f2f89ac5a43c27e8f6e88d0dd7b9e3777cbb2d8e
Kiran <kiran.bacsa@sec.ethz.ch>
2022-03-21 21:39:33 +0800

    update dissipative scripts

--- cluster_scripts/linear_dissipative.sh
+++ cluster_scripts/linear_dissipative.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics linear -type dissipative -ndof 2 -noise 0.20 -n_iter 500
+python gen_config.py -rp "$PWD" -type free -dynamics linear -type dissipative -ndof 2 -noise 0.30 -n_iter 500
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_dissipative.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_linear_dissipative.ini" \
  --dissipative \
  -e 18 -ne 2 -tr 15 -ph 85 -pl 2 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
+ -ord 2 \
  --headless --comet
