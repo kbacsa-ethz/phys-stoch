8984af4089f3077eddbbc2f4f025f984d9599131
Kiran <kiran.bacsa@sec.ethz.ch>
2022-03-21 11:04:37 +0800

    add ord to cluster scripts

--- cluster_scripts/duffing.sh
+++ cluster_scripts/duffing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.20 -n_iter 500
+python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.30 -n_iter 500 \
+-l_x 0 -u_x 1 -l_y 0 -u_y 1
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini" \
  -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
+ -ord 2 \
  --headless --comet
--- cluster_scripts/linear.sh
+++ cluster_scripts/linear.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics linear -ndof 2 -noise 0.20 -n_iter 500
+python gen_config.py -rp "$PWD" -type free -dynamics linear -ndof 2 -noise 0.30 -n_iter 500 \
+-l_x 0 -u_x 1 -l_y 0 -u_y 1
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free.ini" \
  -e 18 -ne 2 -tr 15 -ph 85 -pl 2 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
+ -ord 2 \
  --headless --comet
