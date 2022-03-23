02a450a1bb37f536fd5b66058ee5ddcf7acc95de
Kiran Bacsa <kbacsa-ethz>
2022-03-23 10:35:24 +0800

    change duffing amplitude

--- cluster_scripts/duffing_dissipative_sinusoidal.sh
+++ cluster_scripts/duffing_dissipative_sinusoidal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type dissipative -dynamics duffing -ext sinusoidal -ndof 2 -noise 0.30 -n_iter 500
+python gen_config.py -rp "$PWD" -type dissipative -dynamics duffing -ext sinusoidal -amplitude 1.0 -ndof 2 -noise 0.30 -n_iter 500
 python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_sinusoidal_dissipative.ini"
 python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_sinusoidal_dissipative.ini" \
  -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
