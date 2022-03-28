c442d5ccd77b89dcefe5ed692cd9b2d742b844fe
Kiran <kiran.bacsa@sec.ethz.ch>
2022-03-28 09:29:15 +0800

    added cluster script for linear dissipative encoder comparison

---
+++ cluster_scripts/compare_linear_free_dissipative.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Generate configuration and data
+python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.20 -n_iter 500 -l_x 0. -l_y 0. -u_x 2.0 -u_y 2.0
+python simulation.py --root-path "$PWD" --config-path "config/2springmass_linear_free_dissipative.ini"
+
+# train and test RNN encoder
+python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free_dissipative.ini" \
+ -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
+ -sq 50 -n 20 -wd 0.01 \
+ -tenc "rnn" \
+ --headless --comet
+
+# train and test BiRNN encoder
+python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free_dissipative.ini" \
+ -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
+ -sq 50 -n 20 -wd 0.01 \
+ -tenc "birnn" \
+ --headless --comet
+
+# train and test NODE encoder
+python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free_dissipative.ini" \
+ -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
+ -sq 50 -n 20 -wd 0.01 \
+ -tenc "node" \
+ --headless --comet
+
+# train and test Symplectic NODE
+python train.py --root-path "$PWD" --config-path "config/2springmass_linear_free_dissipative.ini" \
+ -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
+ -sq 50 -n 20 -wd 0.01 \
+ --headless --comet
