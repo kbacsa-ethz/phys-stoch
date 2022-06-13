d1d3b2ce6e719d70caeec3c74469725e04cdbb53
Kiran Bacsa <kbacsa-ethz>
2022-06-13 15:33:54 +0800

    use same force on both wheels

--- simulation.py
+++ simulation.py
@@ -105,6 +105,7 @@ def main(ag, cfg):
 
         exp_amp = force_amp * (0.25 + np.random.random())
         exp_freq = force_freq * (0.25 + np.random.random())
+        case = random.random() < 0.5
         for dof in force_dof:
             if dof == 0:
                 shift = 0
@@ -124,7 +125,6 @@ def main(ag, cfg):
                 trap_force[trap_force > exp_amp] = exp_amp
                 force_input[dof, :] = trap_force
             elif force_type == 'road':
-                case = random.random() > 0.5
                 if case:
                     force_input[dof, :] = exp_amp / 2 * signal.sawtooth(2 * np.pi * exp_freq * tics - shift,
                                                                         width=0.5) + exp_amp / 2
