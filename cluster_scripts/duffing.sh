a42ca547de5db74973f3bcad68f2fdd449c956b3
Kiran Bacsa <kbacsa-ethz>
2022-06-23 09:40:48 +0800

    modified cluster scripts to partial observations

--- cluster_scripts/duffing.sh
+++ cluster_scripts/duffing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics duffing -ndof 2 -noise 0.30 -n_iter 500 \
+python gen_config.py -rp "$PWD" -type free -ext free -dynamics duffing -ndof 2 -noise 0.30 -n_iter 500 -select "partial" \
 -l_x 0 -u_x 1 -l_y 0 -u_y 1
-python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini"
-python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_free.ini" \
+python simulation.py --root-path "$PWD" --config-path "config/2springmass_duffing_free_free.ini"
+python train.py --root-path "$PWD" --config-path "config/2springmass_duffing_free_free.ini" \
  -e 14 -ne 1 -tr 36 -ph 54 -pl 0 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
  -ord 2 \
--- cluster_scripts/linear_3.sh
+++ cluster_scripts/linear_3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python gen_config.py -rp "$PWD" -type free -dynamics linear -ndof 3 -noise 0.30 -n_iter 500 \
+python gen_config.py -rp "$PWD" -type free -ext free -dynamics linear -ndof 3 -noise 0.30 -n_iter 500 \
 -l_x 0 -u_x 1 -l_y 0 -u_y 1
-python simulation.py --root-path "$PWD" --config-path "config/3springmass_linear_free.ini"
-python train.py --root-path "$PWD" --config-path "config/3springmass_linear_free.ini" \
+python simulation.py --root-path "$PWD" --config-path "config/3springmass_linear_free_free.ini"
+python train.py --root-path "$PWD" --config-path "config/3springmass_linear_free_free.ini" \
  -e 29 -ne 1 -tr 24 -ph 11 -pl 4 -nenc 1 \
  -sq 50 -n 30 -wd 0.01 \
  -ord 2 \
